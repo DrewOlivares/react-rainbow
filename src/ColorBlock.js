066c5a3a35c06cf9086d7cb99c2c1814981e2c62
Andrew Olivares <Wrestler533438@gmail.com>
2023-03-08 00:55:21 -0800

    when typing a valid color in the input box the color is added to the array

---
+++ src/ColorBlock.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function ColorBlock(props){
+    return(
+        <div className="colorBlock" style={{'backgroundColor': props.color}}>
+            <p>{props.color}</p>
+        </div>
+    )
+}
